52b9790a218b3f531992e32a62f3cda66dc638cd
kozo <kozo.sakuragawa@gmail.com>
2019-06-02 02:02:57 +0900

    tweaks

--- src/Searchable.php
+++ src/Searchable.php
@@ -25,49 +25,9 @@ class Searchable {
                 continue;
             }
 
-            //if ($field == 'hoge') {
             $filter = FilterFactory::create($options['type']);
             $filter->setOptions($options);
             $filter->process($builder, $field, $value);
-            //}
-            //$builder = $this->createSearchField($builder, $field, $query[$field], $options);
-        }
-
-        return $builder;
-    }
-
-    private function createSearchField(Builder $builder, $field, $value, $options)
-    {
-        switch ($options['type']) {
-            case 'text':
-                if (is_array($value)) {
-                    $builder = $builder->where($field, $value[0], $value[1]);
-                } else {
-                    $builder = $builder->where($field, 'like', '%' . $value . '%');
-                }
-                break;
-
-            case 'bool':
-            case 'integer':
-                $builder = $builder->where($field, $value);
-                break;
-
-            case 'date':
-                $builder = $builder->where($field, $options['comparison'], $value);
-                break;
-
-            case 'callback':
-                $method = $options['method'];
-                $builder = $this->{$method}($builder, $field, $value);
-                break;
-
-            default:
-                if (is_array($value)) {
-                    $builder = $builder->where($field, $value[0], $value[1]);
-                } else {
-                    $builder = $builder->where($field, 'like', '%' . $value . '%');
-                }
-                break;
         }
 
         return $builder;
