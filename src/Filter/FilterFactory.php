24a186dd8dde535e483a5addb12e487944164633
kozo <kozo.sakuragawa@gmail.com>
2019-06-02 18:53:20 +0900

    Add custom filter.

--- src/Filter/FilterFactory.php
+++ src/Filter/FilterFactory.php
@@ -4,6 +4,10 @@ namespace Search\Filter;
 class FilterFactory {
     public static function create($name) : FilterInterface
     {
+        if (class_exists($name)) {
+            return new $name();
+        }
+
         $class = studly_case($name);
         $class = __NAMESPACE__ . '\\' . $class;
 
