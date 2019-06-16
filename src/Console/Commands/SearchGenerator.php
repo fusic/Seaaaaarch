a8d413d41c396823edb2f6917c941f91ac449e14
kozo <kozo.sakuragawa@gmail.com>
2019-06-16 18:45:55 +0900

    Support generate command.

---
+++ src/Console/Commands/SearchGenerator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Search\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class SearchGenerator extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:searchable {name} {--f|force}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new serchable class';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Searchable';
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\Search';
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return  __DIR__ . '/Stubs/Searchable.stab';
+    }
+}
--- src/Providers/SearchServiceProvider.php
+++ src/Providers/SearchServiceProvider.php
@@ -4,6 +4,7 @@ namespace Search\Providers;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\ServiceProvider;
+use Search\Console\Commands\SearchGenerator;
 use Search\Searchable;
 
 class SearchServiceProvider extends ServiceProvider
@@ -16,6 +17,7 @@ class SearchServiceProvider extends ServiceProvider
     public function register()
     {
         $this->registerMacro();
+        $this->registerCommand();
     }
 
     /**
@@ -33,5 +35,16 @@ class SearchServiceProvider extends ServiceProvider
         Builder::macro('search', function (Searchable $search, $query = null) {
             return $search->process($this, $query);
         });
+    }
+
+    protected function registerCommand()
+    {
+        $this->app->singleton('make.searchable', function ($app) {
+            return new SearchGenerator($app['files']);
+        });
+
+        $this->commands([
+            'make.searchable'
+        ]);
     }
 }
